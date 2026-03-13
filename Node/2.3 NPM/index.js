3c5c70416050646e0e75d08346e280a26d29052e
poppy47 <sukhdevps04@gmail.com>
2026-03-13 14:42:02 +0530

    node

--- Node/2.3 NPM/index.js
+++ Node/2.3 NPM/index.js
@@ -1,1 +1,9 @@
-import { generateName } from "sillyname" 
+import  generateName  from "sillyname" ; 
+const name = generateName() ; 
+console.log(`my name is ${name}`); 
+
+
+import superheroes from "superheroes"
+const sname = superheroes;  
+
+console.log(`My superhero name is ${sname}`)
---
+++ Node/2.4 QR Code Project/index.js
@@ -0,0 +1,31 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+
+import inquirer from "inquirer";
+import fs from "fs"; 
+import qr from "qr-image";
+
+inquirer
+    .prompt([{
+        name:"URL", 
+        message:"Enter the URL:"
+    }])
+    .then((answer)=>{
+        const url = answer.URL;
+        var qr_svg = qr.image(url); 
+        qr_svg.pipe(fs.createWriteStream("qr_img.png")); 
+        
+        fs.writeFile("URL.txt", url, (err)=> {
+            if (err) throw err; 
+            console.log("The file has been saved!"); 
+        })
+    }).catch((error)=>{
+        if (error.isTtyError){
+
+        }else{
+
+        }
+    }); 
