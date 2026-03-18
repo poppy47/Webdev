def800ed7df6bb26537cd8e7bbd0f7fad2b0af9b
poppy47 <sukhdevps04@gmail.com>
2026-03-18 16:02:28 +0530

    updating

--- Node/2.2 Native Modules/index.js
+++ Node/2.2 Native Modules/index.js
@@ -1,9 +1,9 @@
 const fs  = require("fs") 
 
-// fs.writeFile("message.txt", "Hello from nodejs!", (err)=> {
-//     if(err) throw err; 
-//     console.log("The file has been saved !"); 
-// })
+fs.writeFile("message.txt", "Hello from nodejs!", (err)=> {
+    if(err) throw err; 
+    console.log("The file has been saved !"); 
+})
 
 fs.readFile("message.txt","utf8",(err, data)=>{
     if(err) throw err; 
